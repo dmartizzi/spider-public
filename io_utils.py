4613b2629f1dc63fd5b67ac8a40f70234221ba71
Davide Martizzi <davide.martizzi@gmail.com>
2025-07-06 13:21:23 -0700

    Code formatting.

--- io_utils.py
+++ io_utils.py
@@ -6,14 +6,17 @@ import h5py
 
 ### DAVIDE MARTIZZI - NOV 2, 2017
 ### THIS MODULE CONTAINS USEFUL
-### FUNCTIONS AND CLASSES TO 
-### READ THE ILLUSTRIS DATASET 
-### AND TO OPERATE ON CARTESIAN 
-### GRIDS. 
+### FUNCTIONS AND CLASSES TO
+### READ THE ILLUSTRIS DATASET
+### AND TO OPERATE ON CARTESIAN
+### GRIDS.
+
 
 class CartesianGrid:
     # a class that generates cartesian grids
-    def __init__(self, L=1.0, n_x=1, n_y=1, n_z=1, time=1.0, hubble=1.0, omega0=0.3, omegaL=0.7):
+    def __init__(
+        self, L=1.0, n_x=1, n_y=1, n_z=1, time=1.0, hubble=1.0, omega0=0.3, omegaL=0.7
+    ):
         self.L = L
         self.n_x = n_x
         self.n_y = n_y
@@ -22,9 +25,9 @@ class CartesianGrid:
         self.hubble = hubble
         self.omega0 = omega0
         self.omegaL = omegaL
-        self.grid = np.zeros((n_x,n_y,n_z))
+        self.grid = np.zeros((n_x, n_y, n_z))
 
-    def CIC(self,pos,var,iblock):
+    def CIC(self, pos, var, iblock):
         # PERFORM CIC INTERPOLATION
         L = self.L
         n_x = self.n_x
@@ -33,113 +36,120 @@ class CartesianGrid:
 
         # indices of the particles
         xp = np.empty(pos.shape)
-        xp[:,0] = pos[:,0]*n_x/L
-        xp[:,1] = pos[:,1]*n_y/L
-        xp[:,2] = pos[:,2]*n_z/L
-        # indices of the particles
-        xp = np.empty(pos.shape)
-        xp[:,0] = pos[:,0]*n_x/L
-        xp[:,1] = pos[:,1]*n_y/L
-        xp[:,2] = pos[:,2]*n_z/L
+        xp[:, 0] = pos[:, 0] * n_x / L
+        xp[:, 1] = pos[:, 1] * n_y / L
+        xp[:, 2] = pos[:, 2] * n_z / L
 
-        temp_grid = np.zeros((n_x,n_y,n_z))
+        temp_grid = np.zeros((n_x, n_y, n_z))
 
         # Perform CIC
-        for x, y, z, v in zip(xp[:,0], xp[:,1], xp[:,2], var[:]):
-            #Weights for CIC
-            dx = x-np.floor(x)
-            dy = y-np.floor(y)
-            dz = z-np.floor(z)
-            tx = -(dx-1.0)
-            ty = -(dy-1.0)
-            tz = -(dz-1.0)
+        for x, y, z, v in zip(xp[:, 0], xp[:, 1], xp[:, 2], var[:]):
+            # Weights for CIC
+            dx = x - np.floor(x)
+            dy = y - np.floor(y)
+            dz = z - np.floor(z)
+            tx = -(dx - 1.0)
+            ty = -(dy - 1.0)
+            tz = -(dz - 1.0)
 
             ix = int(x)
             iy = int(y)
             iz = int(z)
-            temp_grid[ix,iy,iz] += tx*ty*tz*v
+            temp_grid[ix, iy, iz] += tx * ty * tz * v
 
-            ix = int(x)+1
+            ix = int(x) + 1
             iy = int(y)
             iz = int(z)
-            if ix == n_x: ix = 0 # periodic BC
-            temp_grid[ix,iy,iz] += dx*ty*tz*v
+            if ix == n_x:
+                ix = 0  # periodic BC
+            temp_grid[ix, iy, iz] += dx * ty * tz * v
 
             ix = int(x)
-            iy = int(y)+1
+            iy = int(y) + 1
             iz = int(z)
-            if iy == n_y: iy = 0 # periodic BC
-            temp_grid[ix,iy,iz] += tx*dy*tz*v
+            if iy == n_y:
+                iy = 0  # periodic BC
+            temp_grid[ix, iy, iz] += tx * dy * tz * v
 
-            ix = int(x)+1
-            iy = int(y)+1
+            ix = int(x) + 1
+            iy = int(y) + 1
             iz = int(z)
-            if ix == n_x: ix = 0 # periodic BC
-            if iy == n_y: iy = 0 # periodic BC
-            temp_grid[ix,iy,iz] += dx*dy*tz*v
+            if ix == n_x:
+                ix = 0  # periodic BC
+            if iy == n_y:
+                iy = 0  # periodic BC
+            temp_grid[ix, iy, iz] += dx * dy * tz * v
 
             ix = int(x)
             iy = int(y)
-            iz = int(z)+1
-            if iz == n_z: iz = 0 # periodic BC
-            temp_grid[ix,iy,iz] += tx*ty*dz*v
+            iz = int(z) + 1
+            if iz == n_z:
+                iz = 0  # periodic BC
+            temp_grid[ix, iy, iz] += tx * ty * dz * v
 
-            ix = int(x)+1
+            ix = int(x) + 1
             iy = int(y)
-            iz = int(z)+1
-            if ix == n_x: ix = 0 # periodic BC
-            if iz == n_z: iz = 0 # periodic BC
-            temp_grid[ix,iy,iz] += dx*ty*dz*v
+            iz = int(z) + 1
+            if ix == n_x:
+                ix = 0  # periodic BC
+            if iz == n_z:
+                iz = 0  # periodic BC
+            temp_grid[ix, iy, iz] += dx * ty * dz * v
 
             ix = int(x)
-            iy = int(y)+1
-            iz = int(z)+1
-            if iy == n_y: iy = 0 # periodic BC
-            if iz == n_z: iz = 0 # periodic BC
-            temp_grid[ix,iy,iz] += tx*dy*dz*v
+            iy = int(y) + 1
+            iz = int(z) + 1
+            if iy == n_y:
+                iy = 0  # periodic BC
+            if iz == n_z:
+                iz = 0  # periodic BC
+            temp_grid[ix, iy, iz] += tx * dy * dz * v
 
-            ix = int(x)+1
-            iy = int(y)+1
-            iz = int(z)+1
-            if ix == n_x: ix = 0 # periodic BC
-            if iy == n_y: iy = 0 # periodic BC
-            if iz == n_z: iz = 0 # periodic BC
-            temp_grid[ix,iy,iz] += dx*dy*dz*v
+            ix = int(x) + 1
+            iy = int(y) + 1
+            iz = int(z) + 1
+            if ix == n_x:
+                ix = 0  # periodic BC
+            if iy == n_y:
+                iy = 0  # periodic BC
+            if iz == n_z:
+                iz = 0  # periodic BC
+            temp_grid[ix, iy, iz] += dx * dy * dz * v
 
         self.grid = self.grid + temp_grid
         del temp_grid
 
-        print "CIC INTERPOLATION DONE FOR BLOCK ", iblock
+        print("CIC INTERPOLATION DONE FOR BLOCK ", iblock)
 
-    def write_to_hdf5(self,fname):
+    def write_to_hdf5(self, fname):
         ### WRITE OUTPUT IN HDF5
         f = h5py.File(fname, "w")
 
-        time_out = f.create_dataset("time", (1,), dtype='f')
+        time_out = f.create_dataset("time", (1,), dtype="f")
         time_out[...] = self.time
 
-        hubble_out = f.create_dataset("hubble", (1,), dtype='f')
+        hubble_out = f.create_dataset("hubble", (1,), dtype="f")
         hubble_out[...] = self.hubble
 
-        omega0_out = f.create_dataset("omega0", (1,), dtype='f')
+        omega0_out = f.create_dataset("omega0", (1,), dtype="f")
         omega0_out[...] = self.omega0
 
-        omegaL_out = f.create_dataset("omegaL", (1,), dtype='f')
+        omegaL_out = f.create_dataset("omegaL", (1,), dtype="f")
         omegaL_out[...] = self.omegaL
 
-        L_out = f.create_dataset("boxsize", (1,), dtype='f')
+        L_out = f.create_dataset("boxsize", (1,), dtype="f")
         L_out[...] = self.L
 
         gridshape = np.array([self.n_x, self.n_y, self.n_z])
-        gridshape_out = f.create_dataset("gridshape", data = gridshape, dtype='i')
+        gridshape_out = f.create_dataset("gridshape", data=gridshape, dtype="i")
 
-        MGrid_out = f.create_dataset("MGrid", data = self.grid, dtype='f')
+        MGrid_out = f.create_dataset("MGrid", data=self.grid, dtype="f")
 
         f.close()
 
-    def read_from_hdf5(self,fname):
+    def read_from_hdf5(self, fname):
         f = h5py.File(fname, "r")
-        #for name in f:
+        # for name in f:
         #    print name
         self.time = f["time"][0]
         self.hubble = f["hubble"][0]
@@ -149,47 +159,52 @@ class CartesianGrid:
         self.n_x = f["gridshape"][0]
         self.n_y = f["gridshape"][1]
         self.n_z = f["gridshape"][2]
-        
+
         self.grid = f["MGrid"][...]
-        
+
         f.close()
 
-
-    def find_class(self,x,y,z):
+    def find_class(self, x, y, z):
         nx = self.n_x
         ny = self.n_y
         nz = self.n_z
         L = self.L
-        dx = L/nx
-        dy = L/ny
-        dz = L/nz
-        i = np.floor(x/dx)
-        j = np.floor(y/dy)
-        k = np.floor(z/dz)
+        dx = L / nx
+        dy = L / ny
+        dz = L / nz
+        i = np.floor(x / dx)
+        j = np.floor(y / dy)
+        k = np.floor(z / dz)
         i = i.astype(int)
         j = j.astype(int)
         k = k.astype(int)
-        iii = i > nx-1 
-        i[iii] = i[iii]-nx
-        jjj = j > ny-1
-        j[jjj] = j[jjj]-ny
-        kkk = k > nz-1 
-        k[kkk] = k[kkk]-nz
+        iii = i > nx - 1
+        i[iii] = i[iii] - nx
+        jjj = j > ny - 1
+        j[jjj] = j[jjj] - ny
+        kkk = k > nz - 1
+        k[kkk] = k[kkk] - nz
         class_w = np.empty((len(x)))
-        class_w[:] = self.grid[i[:],j[:],k[:]]
+        class_w[:] = self.grid[i[:], j[:], k[:]]
         return class_w
 
+
 #### READ ILLUSTRIS USING MARK VOGELSBERGER'S LIBRARY
-def read_block_vars(fname,ptype):
-    if ptype == 0: print "Processing GAS PARTICLES"
-    if ptype == 1: print "Processing DM PARTICLES"
-    if ptype == 4: print "Processing STARS+WIND PARTICLES"
-    if ptype == 5: print "Processing BH PARTICLES"
+def read_block_vars(fname, ptype):
+    if ptype == 0:
+        print("Processing GAS PARTICLES")
+    if ptype == 1:
+        print("Processing DM PARTICLES")
+    if ptype == 4:
+        print("Processing STARS+WIND PARTICLES")
+    if ptype == 5:
+        print("Processing BH PARTICLES")
     # position
-    rrr = rs.read_block(fname, "POS ", parttype = ptype)
+    rrr = rs.read_block(fname, "POS ", parttype=ptype)
     # potential
-    #pot = rs.read_block(fname, "POT ", parttype = ptype)
-    mmm = rs.read_block(fname, "MASS", parttype = ptype)
+    # pot = rs.read_block(fname, "POT ", parttype = ptype)
+    mmm = rs.read_block(fname, "MASS", parttype=ptype)
     return rrr, mmm
+
 
 ### FUNCTION AND CLASSES END
--- spider_make_mass_grid.py
+++ spider_make_mass_grid.py
@@ -6,9 +6,9 @@ import h5py
 from io_utils import *
 
 ### DAVIDE MARTIZZI - NOV 2, 2017
-### THIS SCRIPT GENERATES A 3D CUBE WITH MASSES AT THE 
-### NODES OF A CARTESIAN GRID. PARTICLES ARE INTERPOLATED 
-### TO THE GRID USING CIC INTERPOLATION. 
+### THIS SCRIPT GENERATES A 3D CUBE WITH MASSES AT THE
+### NODES OF A CARTESIAN GRID. PARTICLES ARE INTERPOLATED
+### TO THE GRID USING CIC INTERPOLATION.
 
 if __name__ == "__main__":
 
@@ -17,121 +17,129 @@ if __name__ == "__main__":
     rank = comm.Get_rank()
     size = comm.Get_size()
 
-    # IllustrisTNG100 Snapshot 099 (z=0) 
-    #path_tng100_z0 = "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_099/snap_099"
-    #path_tng100_z02 = "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_084/snap_084"
-    #path_tng100_z05 = "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_067/snap_067"
-    #path_tng100_z1 = "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_050/snap_050"
-    #path_tng100_z2 = "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_033/snap_033"
-    #path_tng100_z3 = "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_025/snap_025"
-    #path_tng100_z4 = "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_021/snap_021"
-    path_tng100_z5 = "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_017/snap_017"
-    #path_tng100_z6 = "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_013/snap_013"
-    #path_tng100_z8 = "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_008/snap_008"
+    # IllustrisTNG100 Snapshot 099 (z=0)
+    # path_tng100_z0 = "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_099/snap_099"
+    # path_tng100_z02 = "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_084/snap_084"
+    # path_tng100_z05 = "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_067/snap_067"
+    # path_tng100_z1 = "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_050/snap_050"
+    # path_tng100_z2 = "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_033/snap_033"
+    # path_tng100_z3 = "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_025/snap_025"
+    # path_tng100_z4 = "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_021/snap_021"
+    path_tng100_z5 = (
+        "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_017/snap_017"
+    )
+    # path_tng100_z6 = "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_013/snap_013"
+    # path_tng100_z8 = "/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/output/snapdir_008/snap_008"
 
     # Read header
-    #header = rs.snapshot_header(path_tng100_z0+".1.hdf5")
-    #header = rs.snapshot_header(path_tng100_z02+".1.hdf5")
-    #header = rs.snapshot_header(path_tng100_z05+".1.hdf5")
-    #header = rs.snapshot_header(path_tng100_z1+".1.hdf5")
-    #header = rs.snapshot_header(path_tng100_z2+".1.hdf5")
-    #header = rs.snapshot_header(path_tng100_z3+".1.hdf5")
-    #header = rs.snapshot_header(path_tng100_z4+".1.hdf5")
-    header = rs.snapshot_header(path_tng100_z5+".1.hdf5")
-    #header = rs.snapshot_header(path_tng100_z6+".1.hdf5")
-    #header = rs.snapshot_header(path_tng100_z8+".1.hdf5")
+    # header = rs.snapshot_header(path_tng100_z0+".1.hdf5")
+    # header = rs.snapshot_header(path_tng100_z02+".1.hdf5")
+    # header = rs.snapshot_header(path_tng100_z05+".1.hdf5")
+    # header = rs.snapshot_header(path_tng100_z1+".1.hdf5")
+    # header = rs.snapshot_header(path_tng100_z2+".1.hdf5")
+    # header = rs.snapshot_header(path_tng100_z3+".1.hdf5")
+    # header = rs.snapshot_header(path_tng100_z4+".1.hdf5")
+    header = rs.snapshot_header(path_tng100_z5 + ".1.hdf5")
+    # header = rs.snapshot_header(path_tng100_z6+".1.hdf5")
+    # header = rs.snapshot_header(path_tng100_z8+".1.hdf5")
     if rank == 0:
-        print "Time = ", header.time
-        print "H0 = ", header.hubble
-        print "Omega0 = ", header.omega0
-        print "OmegaLambda = ", header.omegaL
-        print "Box Size = ", header.boxsize
-        print "Number of files = ", header.filenum
+        print("Time = ", header.time)
+        print("H0 = ", header.hubble)
+        print("Omega0 = ", header.omega0)
+        print("OmegaLambda = ", header.omegaL)
+        print("Box Size = ", header.boxsize)
+        print("Number of files = ", header.filenum)
 
     # Interpolation grid parameters
-    #nx = 256
-    #ny = 256
-    #nz = 256
-    nx = 512 
-    ny = 512 
-    nz = 512 
-    #nx = 1024
-    #ny = 1024
-    #nz = 1024
+    # nx = 256
+    # ny = 256
+    # nz = 256
+    nx = 512
+    ny = 512
+    nz = 512
+    # nx = 1024
+    # ny = 1024
+    # nz = 1024
     time = header.time
     hubble = header.hubble
     omega0 = header.omega0
-    omegaL = header.omegaL 
+    omegaL = header.omegaL
     L = header.boxsize
-    MGrid = CartesianGrid(L,nx,ny,nz,time,hubble,omega0,omegaL)
+    MGrid = CartesianGrid(L, nx, ny, nz, time, hubble, omega0, omegaL)
 
     ### START LOOP ON SNAPSHOT BLOCKS ###
-    nblocks = header.filenum #number of HDF5 blocks 
-    n_chunks = size #we split the number of blocks in n_chunks = size of the MPI job
-    l_chunks = int(nblocks/size) #average length of each chunk = number of HDF5 blocks per chunk 
-    n_left = nblocks - n_chunks*l_chunks #leftovers
-    l_chunks_arr = np.empty(n_chunks) # length of each chunk + leftovers
+    nblocks = header.filenum  # number of HDF5 blocks
+    n_chunks = size  # we split the number of blocks in n_chunks = size of the MPI job
+    l_chunks = int(
+        nblocks / size
+    )  # average length of each chunk = number of HDF5 blocks per chunk
+    n_left = nblocks - n_chunks * l_chunks  # leftovers
+    l_chunks_arr = np.empty(n_chunks)  # length of each chunk + leftovers
     l_chunks_arr[rank] = l_chunks
-    for i in range(0,n_left): #redistribute leftover chunks among the other cores
+    for i in range(0, n_left):  # redistribute leftover chunks among the other cores
         l_chunks_arr[rank] = int(l_chunks_arr[rank] + 1)
-    istart = int(rank*l_chunks) #starting point of loop on each cpu
-    for iblock in range(istart,istart+int(l_chunks_arr[rank])):
+    istart = int(rank * l_chunks)  # starting point of loop on each cpu
+    for iblock in range(istart, istart + int(l_chunks_arr[rank])):
         ### STARTING OPERATIONS ON i-TH BLOCK
-        if iblock < istart+l_chunks: iii = iblock
-        if iblock == istart+l_chunks: iii = n_chunks*l_chunks+rank # this only exists for a chunk that received a leftover
-    
-        print "BLOCK NO. ", iii
+        if iblock < istart + l_chunks:
+            iii = iblock
+        if iblock == istart + l_chunks:
+            iii = (
+                n_chunks * l_chunks + rank
+            )  # this only exists for a chunk that received a leftover
+
+        print("BLOCK NO. ", iii)
         ### LOOP ON MASSIVE PARTICLE TYPES
-        for ptype in [0, 1, 4]: # , 5]:
+        for ptype in [0, 1, 4]:  # , 5]:
             ### READ POSITION AND POTENTIAL
-            #fname = path_tng100_z0+"."+str(iii)+".hdf5"
-            #fname = path_tng100_z02+"."+str(iii)+".hdf5"
-            #fname = path_tng100_z05+"."+str(iii)+".hdf5"
-            #fname = path_tng100_z1+"."+str(iii)+".hdf5"
-            #fname = path_tng100_z2+"."+str(iii)+".hdf5"
-            #fname = path_tng100_z3+"."+str(iii)+".hdf5"
-            #fname = path_tng100_z4+"."+str(iii)+".hdf5"
-            fname = path_tng100_z5+"."+str(iii)+".hdf5"
-            #fname = path_tng100_z6+"."+str(iii)+".hdf5"
-            #fname = path_tng100_z8+"."+str(iii)+".hdf5"
+            # fname = path_tng100_z0+"."+str(iii)+".hdf5"
+            # fname = path_tng100_z02+"."+str(iii)+".hdf5"
+            # fname = path_tng100_z05+"."+str(iii)+".hdf5"
+            # fname = path_tng100_z1+"."+str(iii)+".hdf5"
+            # fname = path_tng100_z2+"."+str(iii)+".hdf5"
+            # fname = path_tng100_z3+"."+str(iii)+".hdf5"
+            # fname = path_tng100_z4+"."+str(iii)+".hdf5"
+            fname = path_tng100_z5 + "." + str(iii) + ".hdf5"
+            # fname = path_tng100_z6+"."+str(iii)+".hdf5"
+            # fname = path_tng100_z8+"."+str(iii)+".hdf5"
             rrr, mmm = read_block_vars(fname, ptype)
-            #print rrr.shape
-            #print mmm.shape
-            #print rrr.min(),rrr.max()
-            
-            # INTERPOLATE TO GRID USING CIC 
-            MGrid.CIC(rrr,mmm, iii)            
-            #print MGrid.grid
+            # print rrr.shape
+            # print mmm.shape
+            # print rrr.min(),rrr.max()
+
+            # INTERPOLATE TO GRID USING CIC
+            MGrid.CIC(rrr, mmm, iii)
+            # print MGrid.grid
 
             del rrr, mmm
-        
+
         ### END OF LOOP ON MASSIVE PARTICLE TYPES
 
     ### END OF OPERATIONS ON i-TH BLOCK
 
     ### MPI COMMUNICATION TO GET FINAL GRID
-    MGrid_total = CartesianGrid(L,nx,ny,nz,time,hubble,omega0,omegaL)
+    MGrid_total = CartesianGrid(L, nx, ny, nz, time, hubble, omega0, omegaL)
     if size > 1:
-        comm.Allreduce(MGrid.grid, MGrid_total.grid, op = MPI.SUM)
+        comm.Allreduce(MGrid.grid, MGrid_total.grid, op=MPI.SUM)
         comm.Barrier()
         del MGrid
-    else :
+    else:
         MGrid_total.grid = MGrid.grid
         del MGrid
 
     if rank == 0:
-        print MGrid_total
+        print(MGrid_total)
 
-        ### WRITE OUTPUT IN HDF5 
-        #MGrid_total.write_to_hdf5("mass_grid_256.hdf5")
-        #MGrid_total.write_to_hdf5("mass_grid_512.hdf5")
-        #MGrid_total.write_to_hdf5("mass_grid_1024.hdf5")
-        #MGrid_total.write_to_hdf5("mass_grid_512_z02.hdf5")
-        #MGrid_total.write_to_hdf5("mass_grid_512_z05.hdf5")
-        #MGrid_total.write_to_hdf5("mass_grid_512_z1.hdf5")
-        #MGrid_total.write_to_hdf5("mass_grid_512_z2.hdf5")
-        #MGrid_total.write_to_hdf5("mass_grid_512_z3.hdf5")
-        #MGrid_total.write_to_hdf5("mass_grid_512_z4.hdf5")
+        ### WRITE OUTPUT IN HDF5
+        # MGrid_total.write_to_hdf5("mass_grid_256.hdf5")
+        # MGrid_total.write_to_hdf5("mass_grid_512.hdf5")
+        # MGrid_total.write_to_hdf5("mass_grid_1024.hdf5")
+        # MGrid_total.write_to_hdf5("mass_grid_512_z02.hdf5")
+        # MGrid_total.write_to_hdf5("mass_grid_512_z05.hdf5")
+        # MGrid_total.write_to_hdf5("mass_grid_512_z1.hdf5")
+        # MGrid_total.write_to_hdf5("mass_grid_512_z2.hdf5")
+        # MGrid_total.write_to_hdf5("mass_grid_512_z3.hdf5")
+        # MGrid_total.write_to_hdf5("mass_grid_512_z4.hdf5")
         MGrid_total.write_to_hdf5("mass_grid_512_z5.hdf5")
-        #MGrid_total.write_to_hdf5("mass_grid_512_z6.hdf5")
-        #MGrid_total.write_to_hdf5("mass_grid_512_z8.hdf5")
+        # MGrid_total.write_to_hdf5("mass_grid_512_z6.hdf5")
+        # MGrid_total.write_to_hdf5("mass_grid_512_z8.hdf5")
